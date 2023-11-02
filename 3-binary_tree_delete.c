6cd009a3d5f72133f7a1f2aad5ae95568d314f18
Abah-Emmanuel-Adah11 <abahemmanueladah11@gmail.com>
2023-11-02 12:16:36 -0700

    Task 1 to 5

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_tress.h"
+
+/**
+ * binary_tree_delete - A function that deletes a binary tree.
+ * @tree: A pointer to the root node of the tree to delete.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
